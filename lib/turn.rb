55b1deb8755d0c2d879ac1e41a75181aa452e68d
Martin Moy <martinmoy1991@gmail.com>
2019-10-06 16:39:51 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,43 +1,30 @@
-
-
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def input_to_index(input)
- index=input.to_i - 1
+ index = input.to_i - 1
 end
 
-def update_array_at_with(array, index, value)
-  array[index.to_i]=value
-  return array
+
+def move(board, index, value = "X")
+  board[index] = value
+  return board
 end
 
-def move(board, index, value="X")
-  update_array_at_with(board, index, value)
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " " || board[index] == "")
 end
 
-# code your #valid_move? method here
-def valid_move?(board,index)
-  if index.between?(0,8)== true && position_taken?(board,index)==false
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
     return true
   else
-    index.between?(0,8)==false || position_taken?(board,index)== true
     return false
-  end
-end
-
-
-
-def position_taken?(board,index)
-  if board[index]== "" || board[index]== " " || board[index] == nil
-    return false
-  else board[index] == "X" || "O"
-    return true
   end
 end
 
@@ -45,7 +32,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?(board, index)
+  if valid_move?(board,index)
     move(board,index)
     puts display_board(board)
 
